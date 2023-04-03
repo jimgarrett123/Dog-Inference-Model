477233c31c6c4145983baae2b98035ca1f57cdcc
Jim Garrett <jimgarrett123@gmail.com>
2023-04-03 13:29:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@
 # 	curl -X POST -F image=@dog.jpg 'http://localhost:5000/predict'
 
 # import the necessary packages
-from keras.applications import ResNet50
-from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
+from tensorflow..keras.applications import ResNet50
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications import imagenet_utils
 from PIL import Image
 import numpy as np
 import flask
