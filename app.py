ff0b9b0007a2ba24c4a01d5275e0d5f09dad8634
Jim Garrett <jimgarrett123@gmail.com>
2023-04-04 14:24:46 -0400

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,7 @@ def load_model():
 	# substitute in your own networks just as easily)
 	global model
 	model = ResNet50(weights="imagenet")
+	model.call = tf.function(model.call)
 	global graph
 	graph = tf.compat.v1.get_default_graph()
 
@@ -61,8 +62,7 @@ def predict():
 			# classify the input image and then initialize the list
 			# of predictions to return to the client
 			with graph.as_default():
-				preds = tf.function(model.call)
-#				preds = model.predict(image)
+				preds = model.predict(image)
 				results = imagenet_utils.decode_predictions(preds)
 				data["predictions"] = []
 
